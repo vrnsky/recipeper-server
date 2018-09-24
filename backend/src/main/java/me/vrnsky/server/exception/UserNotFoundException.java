77c09da2726b1cdc226b43406c2f989f67c00c51
Yegor Voronyansky <vrnsky@protonmail.ch>
2018-09-24 23:35:20 +0400

    Correct version of the reason of the exception

--- backend/src/main/java/me/vrnsky/server/exception/UserNotFoundException.java
+++ backend/src/main/java/me/vrnsky/server/exception/UserNotFoundException.java
@@ -3,7 +3,7 @@ package me.vrnsky.server.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "You try get not exist recipe")
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "You try get not exist user")
 public class UserNotFoundException extends RuntimeException {
 
     public UserNotFoundException(String message) {
