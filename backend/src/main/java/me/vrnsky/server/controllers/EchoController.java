7587cf778229231d52f174f7fc74af0a9e838ba2
Egor Voronyansky <vrnsky@protonmail.ch>
2019-04-14 22:54:27 +0300

    Changes related to TECH-8

--- backend/src/main/java/me/vrnsky/server/controllers/EchoController.java
+++ backend/src/main/java/me/vrnsky/server/controllers/EchoController.java
@@ -1,11 +1,19 @@
 package me.vrnsky.server.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EchoController {
 
-    public String upStatus() {
-        return "UP";
+    @Autowired
+    private BuildProperties buildProperties;
+
+    @RequestMapping(value = "version", method = RequestMethod.GET)
+    public String getVersion() {
+        return buildProperties.getVersion();
     }
 }
