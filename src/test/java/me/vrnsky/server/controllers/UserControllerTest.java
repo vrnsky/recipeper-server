6959afabf67423e6a39b12d6322674de470c49d5
Voronyansky <vrnsky@protonmail.ch>
2019-05-12 19:10:47 +0400

    TECH #13 fix build by adding auto config annotations on test

--- src/test/java/me/vrnsky/server/controllers/UserControllerTest.java
+++ src/test/java/me/vrnsky/server/controllers/UserControllerTest.java
@@ -6,6 +6,7 @@ import me.vrnsky.server.dto.registration.RegistrationRequest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureTestDatabase
 public class UserControllerTest {
 
     private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
