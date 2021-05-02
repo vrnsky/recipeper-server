e77424f2bd2ea274d46e625e7a355e67010fa90e
Егор Воронянский <vrnsky@protonmail.ch>
2021-05-02 14:57:40 +0800

    #20. Финальные правки

--- src/main/java/me/vrnsky/server/repository/interfaces/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package me.vrnsky.server.repository.interfaces;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepo extends CrudRepository<User, Long> {
-    User findById(int id);
-
-    User findByEmailAndPassword(String email, String password);
-}
