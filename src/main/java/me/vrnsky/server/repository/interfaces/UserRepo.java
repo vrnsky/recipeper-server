968e8dd2ad0c7ead73ed0822a936d8f28d05fd42
Егор Воронянский <vrnsky@protonmail.ch>
2021-05-02 14:42:36 +0800

    #20. Удалил лишние файлы

--- src/main/java/me/vrnsky/server/repository/interfaces/UserRepo.java
+++ src/main/java/me/vrnsky/server/repository/interfaces/UserRepo.java
@@ -1,6 +1,5 @@
 package me.vrnsky.server.repository.interfaces;
 
-import me.vrnsky.server.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepo extends CrudRepository<User, Long> {
